ae7917bd148026e4fc62a64181d2e415167c7e6f
Vijay Patil <109759802+Vijay-Patil20@users.noreply.github.com>
2024-05-11 22:12:58 +0530

    Update auto_backup.sh

--- Automatic_Backup/auto_backup.sh
+++ Automatic_Backup/auto_backup.sh
@@ -1,31 +1,31 @@
 #!/bin/bash
 
 ###############################################################################################
-# Name:auto_backup.sh 
+# Name: auto_backup.sh 
 #-------------------------------------------------------------------------------------------
-# OVERVIEW: This script is created for automate the backup ad compression of a diretory.
+# OVERVIEW: This script is created to automate the backup and compression of a directory.
 #-------------------------------------------------------------------------------------------
 # HISTORY
 #-------------------------------------------------------------------------------------------
 # VERSION || DATE         || AUTHOR           || COMMENTS
 #---------||--------------||------------------||--------------------------------------------
-# 1.0     || 29-sEP-2023  || Vijay Patil      || Initial Version
+# 1.0     || 29-Sep-2023  || Vijay Patil      || Initial Version
 #
 #-------------------------------------------------------------------------------------------
 ###############################################################################################
 
-#The source directoy to be backed up
+#The source directory to be backed up
 source_dir="/home/vijay/test"
 
 #The backup directory destination
 backup_dir="/home/vijay/test.backup"
 
-#If the backup directory is not exits
+#If the backup directory is not present
 if [[ ! -d "$backup_dir" ]]
 then
     mkdir -p "$backup_dir"
 fi
 # copy the source directory to the backup directory
 cp -r "$source_dir" "$backup_dir"
-#Compress the backpup directory
+#Compress the backup directory
 gzip -r "$backup_dir"
